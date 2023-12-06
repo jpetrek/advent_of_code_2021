7d52a59558df23ac85492676ed15c6855e8a1e7a
Petřek <jiri.petrek@thermofisher.com>
2023-12-06 10:49:47 +0100

    Day6

--- Day6_2023.h
+++ Day6_2023.h
@@ -4,9 +4,32 @@
 template<>
 class day<6, 2023> : public day_base<6,2023>
 {
+        size_t calc(size_t t, size_t d)
+        {
+            double D = t * t - 4.0 * d;
+            auto  r1 = static_cast<size_t>(ceil(((static_cast<double>(t) + sqrt(D)) / 2.0) - 1.0));
+            auto  r2 = static_cast<size_t>(floor(((static_cast<double>(t) - sqrt(D)) / 2.0) + 1.0));
+            return  r1 - r2 + 1;
+        }
+
+        size_t join_input(const std::vector<size_t>& input)
+        {
+            std::stringstream ss; 
+            for (const auto& i : input) ss << i;
+            return stoull(ss.str());
+        }
+
         void run_internal() override
         {
-            auto line = input_reader().get_line();
-  
+        
+            auto times = helper::split_and_convert<size_t>(helper::split(input_reader().get_line(), ':')[1], ' ', [](const std::string& a) {return stoull(a); });
+            auto distances = helper::split_and_convert<size_t>(helper::split(input_reader().get_line(), ':')[1], ' ', [](const std::string& a) {return stoull(a); });
+            size_t s1 = 1;
+            for (size_t i = 0; i < times.size(); i++)
+            {
+                s1 *= calc(times[i], distances[i]);
+            }
+            set_star1_result(s1);
+            set_star2_result(calc(join_input(times), join_input(distances)));
         }
 };
