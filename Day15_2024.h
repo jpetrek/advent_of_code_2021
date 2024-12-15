7e82bb865d083242a0ecbc32008c8e0efa9d27e5
Petřek <jiri.petrek@thermofisher.com>
2024-12-15 17:24:38 +0100

    Cleanup

--- Day15_2024.h
+++ Day15_2024.h
@@ -6,6 +6,7 @@ template<>
 class day<15, 2024> : public day_base<15,2024>
 {
     typedef point_2d_generic<long> position;
+    typedef std::set<std::pair<position, char>> unique_position_and_value;
     typedef direction_2d_generic<long> direction;
     typedef std::vector<direction::name> directions;
     typedef std::map<position, char> storage;
@@ -38,7 +39,7 @@ class day<15, 2024> : public day_base<15,2024>
         return {space_2, max_x * 2, max_y, start_2};
     }
 
-    void traverse_crates_1(const position& start, const direction::name move, std::set<std::pair<position, char>>& movable, const storage& space) const
+    void traverse_crates_1(const storage& space, const position& start, const direction::name move, unique_position_and_value& movable) const
     {
         auto new_position = start;
         while ((space.at(new_position) != '#') && (space.at(new_position) != '.'))
@@ -48,22 +49,18 @@ class day<15, 2024> : public day_base<15,2024>
         }
     }
 
-    void traverse_crates_2(const position& start, const direction::name move, std::set<std::pair<position, char>>& movable, const storage& space) const
+    void traverse_crates_2(const storage& space, const position& start, const direction::name move, unique_position_and_value& movable) const
     {
         movable.insert({ start, space.at(start) });
         auto new_position = add(start, move);
         if (space.at(new_position) == '[' || space.at(new_position) == ']')
         {
+            traverse_crates_2(space, new_position, move, movable);
+
             if ((move == direction::up) || (move == direction::down))
             {
-                traverse_crates_2(new_position, move, movable, space);
-
                 new_position.x += space.at(new_position) == '[' ? 1 : -1;
-                traverse_crates_2(new_position, move, movable, space);
-            }
-            else
-            {
-                traverse_crates_2(new_position, move, movable, space);
+                traverse_crates_2(space, new_position, move, movable);
             }
         }
     }
@@ -72,17 +69,17 @@ class day<15, 2024> : public day_base<15,2024>
     {
         auto space = input;
         auto actual = start;
-        for (auto m : moves)
+        for (auto move : moves)
         {
-            std::set<std::pair<position, char>> possible_movable;
-            if (star == 1) traverse_crates_1(actual, m, possible_movable, space);
-            else traverse_crates_2(actual, m, possible_movable, space);
+            unique_position_and_value movable;
+            if (star == 1) traverse_crates_1(space, actual, move, movable);
+            else traverse_crates_2(space, actual, move, movable);
 
-            if (std::all_of(std::begin(possible_movable), std::end(possible_movable), [&](const auto& pm) { return (space.at(add(pm.first, m)) != '#'); }))
+            if (std::all_of(std::begin(movable), std::end(movable), [&](const auto& pm) { return (space.at(add(pm.first, move)) != '#'); }))
             {
-                for (const auto& pm : possible_movable) space[pm.first] = '.';
-                for (const auto& pm : possible_movable) space[add(pm.first, m)] = pm.second;
-                actual = add(actual, m);
+                for (const auto& m : movable) space[m.first] = '.';
+                for (const auto& m : movable) space[add(m.first, move)] = m.second;
+                actual = add(actual, move);
             }
         }
         return space;
@@ -93,7 +90,7 @@ class day<15, 2024> : public day_base<15,2024>
         uint64_t sum = 0;
         for_2D_rectangle_space<long>(0, max_x, 0, max_y, [&space, &sum, id](const auto x, const auto y)
         {
-            if (space.at({ x, y }) == id) sum += 100 * y + x;
+            if (space.at({ x, y }) == id) sum += 100ull * y + x;
         });
         return sum;
     }
@@ -105,24 +102,24 @@ class day<15, 2024> : public day_base<15,2024>
         storage space;
         directions moves;
 
-        input_reader().read_by_line_until_condition_met_or_eof<long>(file_reader::read_condition::empty_line, [&](const auto& line, const long y)
+        input_reader().read_by_line_until_condition_met_or_eof<long>(file_reader::read_condition::empty_line, [&](const auto& line, const auto y)
         {
-                for (long x = 0; x < static_cast<long>(line.size()); x++)
-                {
-                    space[{x,y}] = line[x];
-                    if (line[x] == '@') start_1_pos = { x ,y };
-                }
-                max_x = static_cast<long>(line.size());
-                max_y = y + 1;
+            for (long x = 0; x < static_cast<long>(line.size()); x++)
+            {
+                space[{x,y}] = line[x];
+                if (line[x] == '@') start_1_pos = { x ,y };
+            }
+            max_x = static_cast<long>(line.size());
+            max_y = y + 1;
         });
 
         input_reader().read_by_line_until_condition_met_or_eof<size_t>(file_reader::read_condition::empty_line, [&](const auto& line, const auto)
+        {
+            for (size_t x = 0; x < line.size(); x++)
             {
-                for (size_t x = 0; x < line.size(); x++)
-                {
-                    moves.push_back(direction::char_to_name(line[x]));
-                }
-            });
+                moves.push_back(direction::char_to_name(line[x]));
+            }
+        });
 
         auto [space_2, max_x_2, max_y_2, start_2_pos] = make_input_twice_wide(space, max_x, max_y);
         
--- Day16_2024.h
+++ Day16_2024.h
@@ -7,6 +7,9 @@ class day<16, 2024> : public day_base<16,2024>
 {
     typedef point_2d_generic<long> position;
     typedef direction_2d_generic<long> direction;
+    
+    inline bool is_test() const override { return true; }
+
     void run_internal() override
     {
         input_reader().read_by_line_until_condition_met_or_eof<long>(file_reader::read_condition::empty_line, [&](const auto& line, const long y)
